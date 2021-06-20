f28f3ba9c5ead8993756348070616cf5f8facd56
Ashok812 <chhetriarash81@gmail.com>
2021-06-20 21:38:08 +0545

    Vendor Model and Migration Created

---
+++ database/migrations/2021_06_20_155025_create_vendors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVendorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address')->nullable();
+            $table->string('phone');
+            $table->integer('reg_number');
+            $table->longText('remark')->nullable();
+            $table->foreignId('customer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendors');
+    }
+}
